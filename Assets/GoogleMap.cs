65ae999dd0528a71e3b614e5e5d8d39ad36c7c53
wonhojin <ghgud0748@gmail.com>
2020-12-15 09:53:17 +0900

    navigation

---
+++ Assets/GoogleMap.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoogleMap : MonoBehaviour
+{
+    public RawImage mapRawImage;
+
+    string url;
+
+    public float lat = 35.885100f;
+    public float lon = 128.626949f;
+
+    public int zoom = 18;
+    public int mapWidth = 300;
+    public int mapHeight = 300;
+
+    public enum mapType { road, satellite, hybrid, terrain }
+    public mapType mapSelected;
+    public int scale;
+
+    float monsterLat = 35.885778f;
+    float monsterLon = 128.627866f;
+
+    public GameObject monsterPrefab;
+
+    static float UPDATE_TIME = 3f;
+    float updateTime = UPDATE_TIME;
+
+    IEnumerator Map()
+    {
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon +
+            "&zoom=" + zoom + "&size" + mapWidth + "x" + mapHeight + "&scale" + scale
+            + "&maptype" + mapSelected +
+            "&key=AIzaSyAHA_IVgU-ZXH0iF3rd2gqZQvWf-mTww-E" +
+            "&markers=color:red%7Clabel:C%7C" + lat + "," + lon +
+            "&markers=color:green%7Clabel:C%7C" + monsterLat + "," + monsterLon;
+
+        WWW www = new WWW(url);
+        yield return www;
+        mapRawImage.texture = www.texture;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartGPS());
+
+        mapRawImage = gameObject.GetComponent<RawImage>();
+        StartCoroutine(Map());
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(updateTime >= 0)
+        {
+            updateTime = updateTime - Time.deltaTime;
+        } else
+        {
+            updateTime = UPDATE_TIME;
+
+            float latitude = Input.location.lastData.latitude;
+            float longitude = Input.location.lastData.longitude;
+
+            if(latitude != 0f && longitude != 0)
+            {
+                if(lat != latitude || lon != longitude)
+                {
+                    lat = latitude;
+                    lon = longitude;
+
+                    StartCoroutine(Map());
+
+                    if(Mathf.Abs(lat - monsterLat) < 0.0001f && Mathf.Abs(lon - monsterLon) < 0.0001f)
+                    {
+                        GameObject monster = Instantiate(monsterPrefab);
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator StartGPS()
+    {
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        Input.location.Start();
+
+        int maxWait = 20;
+        while(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if(maxWait < 1)
+        {
+            print("Time Out");
+            yield break;
+        }
+
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        } else
+        {
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+
+        }
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void resetGPS()
+    {
+        lat = 35.885100f;
+        lon = 128.626949f;
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Test : MonoBehaviour
+{
+    public GameObject AObjectPrefab;
+    public GameObject BObjectPrefab;
+    public GameObject CObjectPrefab;
+    public GameObject UserPrefab;
+
+    private GameObject AObject;
+    private GameObject BObject;
+    private GameObject CObject;
+    private GameObject User;
+
+
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+    double x3;
+    double y3;
+
+    double r1;
+    double r2;
+    double r3;
+
+    double S;
+    double T;
+
+
+    double x;
+    double y;
+
+
+    private Vector3 userLocation;
+
+    public List<GameObject> BeconList = new List<GameObject>();
+
+    public void BeconSetting()
+    {
+        if (BeconList.Count != 0)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                DestroyImmediate(BeconList[0]);
+                BeconList.RemoveAt(0);
+            }
+
+            Destroy(User);
+        }
+
+        List<Dictionary<string, object>> data = CSVReader.Read("beacorn name");
+
+
+        AObject = Instantiate(AObjectPrefab);
+        AObject.transform.position = new Vector3(Convert.ToSingle(data[0]["x coor"]), Convert.ToSingle(data[0]["y coor"]), Convert.ToSingle(data[0]["z coor"]));
+        BeconList.Add(AObject);
+
+        BObject = Instantiate(BObjectPrefab);
+        BObject.transform.position = new Vector3(Convert.ToSingle(data[1]["x coor"]), Convert.ToSingle(data[1]["y coor"]), Convert.ToSingle(data[1]["z coor"]));
+        BeconList.Add(BObject);
+
+        CObject = Instantiate(CObjectPrefab);
+        CObject.transform.position = new Vector3(Convert.ToSingle(data[2]["x coor"]), Convert.ToSingle(data[2]["y coor"]), Convert.ToSingle(data[2]["z coor"]));
+        BeconList.Add(CObject);
+
+        User = Instantiate(UserPrefab);
+        User.transform.position = new Vector3(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10), 0);
+
+    }
+
+
+    public void Trilateration()
+    {
+        x1 = AObject.transform.position.x;
+        y1 = AObject.transform.position.y;
+
+        x2 = BObject.transform.position.x;
+        y2 = BObject.transform.position.y;
+
+        x3 = CObject.transform.position.x;
+        y3 = CObject.transform.position.y;
+
+
+        r1 = Vector3.Distance(User.transform.position, AObject.transform.position);
+        r2 = Vector3.Distance(User.transform.position, BObject.transform.position);
+        r3 = Vector3.Distance(User.transform.position, CObject.transform.position);
+
+        S = (Math.Pow(x3, 2) - Math.Pow(x2, 2) + Math.Pow(y3, 2) - Math.Pow(y2, 2)
+            + Math.Pow(r2, 2) - Math.Pow(r3, 2)) / 2;
+
+        T = (Math.Pow(x1, 2) - Math.Pow(x2, 2) + Math.Pow(y1, 2) - Math.Pow(y2, 2)
+            + Math.Pow(r2, 2) - Math.Pow(r1, 2)) / 2;
+
+
+        y = ((T * (x2 - x3)) - (S * (x2 - x1))) / (((y1 - y2) * (x2 - x3)) - ((y3 - y2) * (x2 - x1)));
+        x = ((y * (y1 - y2)) - T) / (x2 - x1);
+
+        userLocation = new Vector3((float)x, (float)y, 0);
+
+        Debug.Log(userLocation);
+    }
